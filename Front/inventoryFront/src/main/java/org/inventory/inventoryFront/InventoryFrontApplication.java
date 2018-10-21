ca76968433d8276b5a36527cb9529f49246f64df
Abraham Rager Gomez Flores <rager.gf@gmail.com>
2018-10-21 15:20:35 -0500

    Se crea inventory Front

---
+++ Front/inventoryFront/src/main/java/org/inventory/inventoryFront/InventoryFrontApplication.java
@@ -0,0 +1,12 @@
+package org.inventory.inventoryFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryFrontApplication.class, args);
+	}
+}
---
+++ Front/inventoryFront/src/main/resources/static/js/service/product_service.js
@@ -0,0 +1,78 @@
+'use strict';
+
+App.factory('ProductService', ['$http', '$q', function($http, $q){
+
+	var server = '192.168.0.9';
+	var port = '8080';
+
+	return {
+		
+			fetchAllProducts: function() {
+					return $http.get('http://'+server+':'+port+'/product/')
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while fetching product');
+										return $q.reject(errResponse);
+									}
+							);
+			},
+		    
+		    createProduct: function(product){
+					return $http.post('http://'+server+':'+port+'/product/', product)
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while creating product');
+										return $q.reject(errResponse);
+									}
+							);
+		    },
+		    
+		    updateProduct: function(product, id){
+					return $http.put('http://'+server+':'+port+'/product/'+id, product)
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while updating product');
+										return $q.reject(errResponse);
+									}
+							);
+			},
+		    
+			deleteProduct: function(id){
+					return $http.delete('http://'+server+':'+port+'/product/'+id)
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while deleting product');
+										return $q.reject(errResponse);
+									}
+							);
+			},
+			
+			searchByProduct: function(barcode){
+					return $http.get('http://'+server+':'+port+'/product/search/findByBarcode?barcode='+barcode)
+					.then(
+									function(response){										
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while deleting product');
+										return $q.reject(errResponse);
+									}
+							);
+					
+			}
+		
+	};
+
+}]);
